dca03423d66b55bf19de9478fc5d3a8d404ba8c9
dorian m <pro.dorianm@gmail.com>
2023-01-29 15:41:42 +0100

    end of challenge

---
+++ site-consultant/js/buttonBurger.js
@@ -0,0 +1,9 @@
+document.getElementById('open').addEventListener('click', () => {
+    document.getElementById('nav').classList.toggle('openMenu');
+})
+
+// document.getElementById('close').addEventListener('click', () => {
+//     document.getElementById('nav').classList.remove();
+// })
+
+console.log(document.getElementById('close'))
